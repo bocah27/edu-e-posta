b14328365c94eec59b01d9a13a69ec0afd0e0281
bocah27 <59579906+bocah27@users.noreply.github.com>
2020-06-14 04:18:17 +0700

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,14 +1,8 @@
 from pages.accountDetails import AccountDetail
 from pages.costa import Costa
 
-print("Hoşgeldiniz, iyi günler geçirmenizi dileriz.")
-print("Bu betikle otomatik .edu e-postaları oluşturabilirsiniz.")
-print("Amerikada olduğunuzu gösteren bir VPN kullanın")
-print("---")
-print("Anwar MEQOR tarafından yazıldı - github.com/AnwarMEQOR")
-print("XORCAN tarafından türkçeleştirildi - github.com/xorcan")
 
-email = input("E-posta adresinizi girin (sahte girmeyin, çalışmaz.) : ")
+email = input("isi email asli : ")
 accountdetails = AccountDetail()
 info = accountdetails.getInfo()
 print(info)
