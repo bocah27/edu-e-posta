f605b887d89c241c8aa2711705dde7eee5bedc43
bocah27 <59579906+bocah27@users.noreply.github.com>
2020-06-14 04:22:30 +0700

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,9 +1,6 @@
 from pages.accountDetails import AccountDetail
 from pages.costa import Costa
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
 email = input("isi email asli : ")
 accountdetails = AccountDetail()
 info = accountdetails.getInfo()
